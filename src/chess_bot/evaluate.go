7a26d337049b87d4f450c8998efed08584c15efd
Adam Rose <adam.p.rose1@gmail.com>
2024-05-04 13:34:39 +0100

    refactor

--- src/chess_bot/evaluate.go
+++ src/chess_bot/evaluate.go
@@ -12,37 +12,37 @@ func Evaluate(gs *gamestate.GameState) float64 {
 	
 	//MoveRAys
 
+	
+	var score float64 = 0.0
+	
+	EvalMoves := evaluate.GetEvalMoveRays(gs)
+	
+	// piece total
+	score += evaluate.EvalPieceCounts(cb)
+	
+	// pawn eval
+	score += evaluate.PawnEval(cb, &gs.MoveRays.PawnCapRays)
+	
+	// knight eval 
+	score += evaluate.KnightEval(cb, EvalMoves.W_kn_rays, EvalMoves.B_kn_rays)
+	
+	// bishop eval
+	score += evaluate.BishopEval(&EvalMoves, cb)
+	
+	// rook eval
+	score += evaluate.RookEval(&EvalMoves, cb)
+	
+	// queen eval
+	score += evaluate.QueenEval(&EvalMoves, cb)
+	
+	// king eval
+	
+	
+	
 	var score_scalar float64 = 1.0
 	if !gs.White_to_move {
 		score_scalar = -1.0
 	}
-
-	var score float64 = 0.0
-
-	EvalMoves := evaluate.GetEvalMoveRays(gs)
-
-	// piece total
-	score += evaluate.EvalPieceCounts(cb)
-
-	// pawn eval
-	score += evaluate.PawnEval(cb, &gs.MoveRays.PawnCapRays)
-
-	// knight eval 
-	score += evaluate.KnightEval(cb, EvalMoves.W_kn_rays, EvalMoves.B_kn_rays)
-
-	// bishop eval
-	score += evaluate.BishopEval(&EvalMoves, cb)
-
-	// rook eval
-	score += evaluate.RookEval(&EvalMoves, cb)
-
-	// queen eval
-	score += evaluate.QueenEval(&EvalMoves, cb)
-
-	// king eval
-
-
-
 	// general
 	// score += score_scalar * float64(len(gs.MoveList)) / 50 // number of moves
 	if gs.InCheck {
